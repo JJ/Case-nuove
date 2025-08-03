d68da69a0f758aefa436f99b551b38eb32992d8f
JJ Merelo <jjmerelo@gmail.com>
2025-08-03 12:56:21 +0200

    closes #5 visualizes timeline

---
+++ script/timeline-doge-case-nuove.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+load("data/ducali_dogi_data.rda")
+
+# create a timeline with the ducali_dogi_data using different colors for different Types
+# The timeline would be a single bar, colored depending of the type of family, with every bar spanning from the year it started (in Year) until the year the next started (also in Year)
+
+# load the library that includes the function "lead"
+
+library(dplyr)
+ggplot(ducali_dogi_data, aes(x = Year, y = 1, color = Type)) +
+  geom_segment(aes(xend = lead(Year, order_by = Year), yend = 1), size = 5) +
+  scale_color_manual(values = c("Ducali"="gold", "Nuovissime" = "blue", "Nuove" = "green", "Apostoliche" = "red", "Evangeliche" = "black")) +
+  labs(title = "Timeline of Doge Families in Venice",
+       x = "Year",
+       y = "") +
+  theme_minimal() +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.title = element_blank())
+
