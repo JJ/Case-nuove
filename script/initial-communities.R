4d923354ef7cc97df1027431569b476dcb0d9d20
JJ Merelo <jjmerelo@gmail.com>
2025-08-05 11:03:19 +0200

    #4 analyzing communities with consolidated weights

--- script/initial-communities.R
+++ script/initial-communities.R
@@ -23,3 +23,33 @@ plot(marriage_graph_before_first_ducale,
 
 save(marriage_graph_before_first_ducale, file = "data/marriage_graph_before_first_ducale.rda")
 
+# take only the main component of the graph
+
+main_component <- components(marriage_graph_before_first_ducale)$membership
+main_component_graph <- subgraph(marriage_graph_before_first_ducale, V(marriage_graph_before_first_ducale)$name[main_component == 1])
+plot(main_component_graph,
+     vertex.size = 5,
+     edge.arrow.size = 0.5,
+     main = "Main Component of Marriages Before the Ducale (1413)"
+)
+
+# consolidate multi-edges into weights
+
+main_component_graph <- simplify(main_component_graph, remove.multiple = TRUE, edge.attr.comb = "sum")
+
+V(main_component_graph)$betweenness <- betweenness(main_component_graph)
+V(main_component_graph)$closeness <- closeness(main_component_graph)
+
+plot(main_component_graph,
+     vertex.size = 1+V(main_component_graph)$betweenness/100,
+     edge.arrow.size = 0.5,
+     main = "Main Component of Marriages Before the Ducale (1413) - Betweenness Centrality"
+)
+
+plot(main_component_graph,
+     vertex.size = V(main_component_graph)$closeness*1000,
+     edge.arrow.size = 0.5,
+     main = "Main Component of Marriages Before the Ducale (1413) - Betweenness Centrality"
+)
+
+
