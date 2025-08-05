21dcb4921a14a75f78a7a9f69d3c1330620b1248
JJ Merelo <jjmerelo@gmail.com>
2025-08-05 10:33:20 +0200

    #4 using the original capitalization

--- script/filter-marriages.R
+++ script/filter-marriages.R
@@ -1,4 +1,4 @@
-load("data/venice-marriages.rda")
+load("data/venice-marriages.Rda")
 
 # eliminate all rows that have no year or no wife name. Eliminate also the marriage_number column
 #
@@ -8,4 +8,4 @@ venice_marriages <- marriages.raw %>%
   filter(!is.na(year) & wife_familyname_std!="") %>%
   select(-marriage_number)
 
-save(venice_marriages, file = "data/venice-marriages.rda")
+save(venice_marriages, file = "data/venice-marriages-filtered.rda")
