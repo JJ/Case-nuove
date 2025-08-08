80558be3f2cbb22b9b7bb18eae34475ca7298279
JJ Merelo <jjmerelo@gmail.com>
2025-08-08 10:26:52 +0200

    closes #4 with 3 generations = 90 years

--- script/network-slices-with-elections.R
+++ script/network-slices-with-elections.R
@@ -4,7 +4,7 @@ library(igraph)
 load("data/venice-marriages-filtered.rda")
 load("data/ducali_dogi_data.rda")
 
-DEPTH_IN_YEARS <- 30
+DEPTH_IN_YEARS <- 90
 
 results_df <- data.frame( Year = integer(),
                           Casata_doge = character(),
