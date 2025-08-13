964d445326e0db8a88759b8d8ae77ae6463ec665
JJ Merelo <jjmerelo@gmail.com>
2025-08-13 19:14:59 +0200

    closes #19 already used in the next function
    This settles the proportion of the nuove family that are in every first community, which in many cases is very high

--- script/network-slices-proportions-ducali.R
+++ script/network-slices-proportions-ducali.R
@@ -1,22 +1,24 @@
-library(dplyr)
-library(igraph)
-
-devtools::load_all() 
+devtools::load_all()
 
 load("data/venice-marriages-filtered.rda")
 load("data/ducali_dogi_data.rda")
+load("data/families.rda")
 
 DEPTH_IN_YEARS <- 90
 
-results_df <- data.frame( Year = integer(),
+proportions_results_df <- data.frame( Year = integer(),
                           Casata_doge = character(),
                           Type = character(),
-                          DogeFamilyBetweennessPosition = integer(),
-                          DogeFamilyClosenessPosition = integer(),
-                          DogeFamilyEigenCentralityPosition = integer(),
-                          TopFamilyBetweenness = character(),
-                          TopFamilyCloseness = character(),
-                          TopFamilyEigenCentrality = character())
+                          ProportionLunghiInFirstCommunity = numeric(),
+                          ProportionDucaliInFirstCommunity = numeric())
+
+# extract from the family.types list those whose value is "Ducali"
+
+ducali_families <- names(family.types)[family.types == "Ducali"]
+lunghi_families <- names(family.types)[family.types == "Apostoliche" | family.types == "Vecchie" | family.types == "Evangeliche"]
+
+ducali_families_number <- length(ducali_families)
+lunghi_families_number <- length(lunghi_families)
 
 for (i in 1:nrow(ducali_dogi_data)) {
   election_year <- ducali_dogi_data$Year[i]
@@ -28,18 +30,31 @@ for (i in 1:nrow(ducali_dogi_data)) {
     election_year = election_year,
     years_before = DEPTH_IN_YEARS
   )
-  
+
   # Extract and simplify the main component of the graph
   main_component_graph <- extract_main_component(marriage_graph_slice)
-  
+
   # Analyze community structure
   community_analysis <- analyze_communities(
     graph = main_component_graph,
     total_families = families_in_graph
   )
-  
-  # Extract the proportion of families in the largest community
-  families_in_first_community <- community_analysis$proportion_in_largest
 
-}
+  # extract the key of the first element in community_analysis$sorted_communities
+  first_community <- names(community_analysis$sorted_communities)[1]
+  families_in_first_community <- unname(community_analysis$communities[first_community])[[1]]
 
+
+  # Intersection between ducali_families and families in the first community
+  ducali_families_in_first_community <- intersect(ducali_families, families_in_first_community)
+  lunghi_families_in_first_community <- intersect(lunghi_families, families_in_first_community)
+
+  results_df <- rbind(results_df,
+                           data.frame(Year = election_year,
+                                      Casata_doge = casata,
+                                      Type = type,
+                                      ProportionLunghiInFirstCommunity = length(lunghi_families_in_first_community) / lunghi_families_number,
+                                      ProportionDucaliInFirstCommunity = length(ducali_families_in_first_community) / ducali_families_number))
+  }
+
+save(proportions_results_df, file = "data/proportions_results_df.rda")
