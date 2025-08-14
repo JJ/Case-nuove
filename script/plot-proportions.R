8c698682d8933b9eeb07ec049507c157e537f06e
JJ Merelo <jjmerelo@gmail.com>
2025-08-14 11:56:47 +0200

    #18 plotting the composition of the first community
    Maybe we're getting it wrong and we need to talk about the nuove family in general, not only the ducali

--- script/plot-proportions.R
+++ script/plot-proportions.R
@@ -15,3 +15,16 @@ ggplot(proportions_results_df, aes(x = Year)) +
   theme_minimal() +
   scale_color_manual(values = c("Lunghi" = "blue", "Ducali" = "red"))
 ggsave("figures/proportions_first_community.png", width = 10, height = 6, dpi = 300)
+
+ggplot(proportions_results_df, aes(x = Year)) +
+  geom_line(aes(y = ProportionLunghiOutOfFirstCommunity, color = "Lunghi")) +
+  geom_line(aes(y = ProportionDucaliOutOfFirstCommunity, color = "Ducali")) +
+  geom_point(aes(y = ProportionLunghiOutOfFirstCommunity, color = Type), size = ifelse(proportions_results_df$Type=="Ducali",0,3)) +
+  geom_point(aes(y = ProportionDucaliOutOfFirstCommunity, color = Type), size = ifelse(proportions_results_df$Type!="Ducali",0,3)) +
+  labs(title = "Proportions of Families in the First Community Over Time",
+       x = "Year",
+       y = "Proportion",
+       color = "Family Type") +
+  theme_minimal() +
+  scale_color_manual(values = c("Lunghi" = "blue", "Ducali" = "red"))
+ggsave("figures/composition_first_community.png", width = 10, height = 6, dpi = 300)
