91a98c8b86a51aea623efdc7c9a933c465ea4857
JJ Merelo <jjmerelo@gmail.com>
2025-08-14 10:54:57 +0200

    #18 plot proportions of the whole type of family in 1st community

---
+++ script/plot-proportions.R
@@ -0,0 +1,17 @@
+devtools::load_all()
+library(ggplot2)
+
+load("data/proportions_results_df.rda")
+
+ggplot(proportions_results_df, aes(x = Year)) +
+  geom_line(aes(y = ProportionLunghiInFirstCommunity, color = "Lunghi")) +
+  geom_line(aes(y = ProportionDucaliInFirstCommunity, color = "Ducali")) +
+  geom_point(aes(y = ProportionLunghiInFirstCommunity, color = Type), size = ifelse(proportions_results_df$Type=="Ducali",0,3)) +
+  geom_point(aes(y = ProportionDucaliInFirstCommunity, color = Type), size = ifelse(proportions_results_df$Type!="Ducali",0,3)) +
+  labs(title = "Proportions of Families in the First Community Over Time",
+       x = "Year",
+       y = "Proportion",
+       color = "Family Type") +
+  theme_minimal() +
+  scale_color_manual(values = c("Lunghi" = "blue", "Ducali" = "red"))
+ggsave("figures/proportions_first_community.png", width = 10, height = 6, dpi = 300)
