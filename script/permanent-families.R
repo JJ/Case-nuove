b3100d565a9114401f0686ab7fa2b53a4b668301
JJ Merelo <jjmerelo@gmail.com>
2025-08-15 13:01:28 +0200

    #22 :sparkles: find the 59 most common families

---
+++ script/permanent-families.R
@@ -0,0 +1,51 @@
+devtools::load_all()
+
+load("data/venice-marriages-filtered.rda")
+load("data/ducali_dogi_data.rda")
+load("data/families.rda")
+
+DEPTH_IN_YEARS <- 90
+
+# extract from the family.types list those whose value is "Ducali"
+
+ducali_families <- names(family.types)[family.types == "Ducali"]
+lunghi_families <- names(family.types)[family.types == "Apostoliche" | family.types == "Vecchie" | family.types == "Evangeliche"]
+nuove_families <- names(family.types)[family.types == "Ducali" | family.types == "Nuove"]
+
+
+first_network <- create_marriage_network(
+  venice_marriages,
+  election_year = ducali_dogi_data$Year[1],
+  years_before = DEPTH_IN_YEARS
+)
+
+permanent_families <- V(first_network)$name
+
+for (i in 2:nrow(ducali_dogi_data)) {
+  election_year <- ducali_dogi_data$Year[i]
+  casata <- ducali_dogi_data$Casata[i]
+
+  marriage_graph_slice <- create_marriage_network(
+    venice_marriages,
+    election_year = election_year,
+    years_before = DEPTH_IN_YEARS
+  )
+
+  permanent_families <- intersect(permanent_families, V(marriage_graph_slice)$name)
+}
+
+# for every family in permanent_families, map it to its type
+#
+
+permanent_families_types <- sapply(permanent_families, function(family) {
+  if (family %in% names(family.types)) {
+    return(family.types[[family]])
+  } else {
+    return(NA)
+  }
+})
+permanent_families_df <- data.frame(
+  Casata = permanent_families,
+  Type = permanent_families_types,
+  stringsAsFactors = FALSE
+)
