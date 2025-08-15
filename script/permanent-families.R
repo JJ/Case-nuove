ca42670e01cb4af708577b897aab03573f79c5a6
JJ Merelo <jjmerelo@gmail.com>
2025-08-15 18:13:04 +0200

    Saving families

--- script/permanent-families.R
+++ script/permanent-families.R
@@ -49,3 +49,42 @@ permanent_families_df <- data.frame(
   Type = permanent_families_types,
   stringsAsFactors = FALSE
 )
+
+
+# Try after the first ducali election
+first_network <- create_marriage_network(
+  venice_marriages,
+  election_year = ducali_dogi_data$Year[2],
+  years_before = DEPTH_IN_YEARS
+)
+
+permanent_families_2 <- V(first_network)$name
+
+for (i in 3:nrow(ducali_dogi_data)) {
+  election_year <- ducali_dogi_data$Year[i]
+  casata <- ducali_dogi_data$Casata[i]
+
+  marriage_graph_slice <- create_marriage_network(
+    venice_marriages,
+    election_year = election_year,
+    years_before = DEPTH_IN_YEARS
+  )
+
+  permanent_families_2 <- intersect(permanent_families_2, V(marriage_graph_slice)$name)
+}
+
+# for every family in permanent_families, map it to its type
+#
+
+permanent_families_types <- sapply(permanent_families_2, function(family) {
+  if (family %in% names(family.types)) {
+    return(family.types[[family]])
+  } else {
+    return(NA)
+  }
+})
+permanent_families_2_df <- data.frame(
+  Casata = permanent_families_2,
+  Type = permanent_families_types,
+  stringsAsFactors = FALSE
+)
