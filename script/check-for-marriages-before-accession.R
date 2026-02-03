137fbbaba173c85a8d446fa01b1bdd97e089d09e
JJ Merelo <jjmerelo@gmail.com>
2026-02-03 12:18:15 +0100

    #29 Fixing some more, Bell'Ossello we assume was present at the Serrata

--- script/check-for-marriages-before-accession.R
+++ script/check-for-marriages-before-accession.R
@@ -1,8 +1,6 @@
 families.group.accession <- read.csv("data/family-labels-accession.csv", stringsAsFactors = FALSE)
 load("data/venice-marriages.Rda")
 
-# for every row in marriages.raw, check if the accession date of husband_familyname_std is after the year of the marriage; indicate that in a new column
-
 library(dplyr)
 marriages.raw %>% 
   rowwise() %>%
@@ -19,9 +17,6 @@ marriages.raw %>%
     )
   ) -> marriages.check.accession 
 
-
-# filter marriages.check.accession to keep only rows where husband_accession_after_marriage or wife_accession_after_marriage is TRUE
-
 marriages.with.accession.issues <- marriages.check.accession %>%
   filter(husband_accession_after_marriage == TRUE | wife_accession_after_marriage == TRUE)
 
@@ -29,3 +24,7 @@ marriages.excluding.accession.issues <- marriages.check.accession %>%
   filter(husband_accession_after_marriage != TRUE & wife_accession_after_marriage != TRUE) %>%
   select(-husband_accession_after_marriage, -wife_accession_after_marriage)
 
+marriage.with.missing.accession <- marriages.check.accession %>%
+  filter(is.na(husband_accession_after_marriage) | is.na(wife_accession_after_marriage)) %>% filter( !is.na(year) & wife_familyname_std!="") 
+
+missing_families <- unique( marriage.with.missing.accession$wife_familyname_std)
